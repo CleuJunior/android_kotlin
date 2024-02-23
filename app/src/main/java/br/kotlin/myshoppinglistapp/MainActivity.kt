03b9976a73ab705ccbe2f305d5b01f4ff586ecdf
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2024-02-23 00:37:27 -0300

    feat: AlertDialog

--- app/src/main/java/br/kotlin/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/br/kotlin/myshoppinglistapp/MainActivity.kt
@@ -3,17 +3,10 @@ package br.kotlin.myshoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import br.kotlin.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,19 +19,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            onClick = {  },
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text(text = "Add Item")
-                        }
-                    }
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
+
